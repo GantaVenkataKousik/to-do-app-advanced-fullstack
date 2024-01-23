5a3535df0645b264e1734ff26f8c12350cbc3fe4
Venkata Kousik <Venkatakousikcse01@gmail.com>
2024-01-23 11:57:01 +0530

    Commit all changes

---
+++ completeTodo.js
@@ -0,0 +1,24 @@
+/* eslint-disable semi */
+// eslint-disable-next-line no-undef
+const argv = require("minimist")(process.argv.slice(2));
+const db = require("./models/index");
+
+const markAsComplete = async (id) => {
+  try {
+    await db.Todo.markComplete(id);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+(async () => {
+  const { id } = argv;
+  if (!id) {
+    throw new Error("Need to pass an id");
+  }
+  if (!Number.isInteger(Number(id))) {
+    throw new Error("The id needs to be an integer");
+  }
+  await markAsComplete(id);
+  await db.Todo.showList();
+})();
